710adbbc5f0eceff66597f40abb1cf20046bbfab
Katrina Apel <kapel2810@gmail.com>
2021-11-23 10:46:03 +0200

    ex 2 done

--- app.js
+++ app.js
@@ -5,15 +5,32 @@ let divElement = document.getElementById('main-container')
 /////////////////////////////
 //----- ex1-------
 
+// const runCode = () => {
+//     console.log('start')
+//     getTime()
+//     console.log('end')
+// }
+
+// const getTime = ()=> {
+//     let now = new Date()
+//     let currentTime = now.toLocaleTimeString()
+
+//     console.log(currentTime)
+// }
+
+///////////////////////
+// -------- ex 2 ----------
 const runCode = () => {
     console.log('start')
     getTime()
     console.log('end')
 }
 
-const getTime = ()=> {
-    let now = new Date()
-    let currentTime = now.toLocaleTimeString()
+const getTime = () => {
+    setTimeout(() => {
+        let now = new Date()
+        let currentTime = now.toLocaleTimeString()
+        console.log(currentTime)
+    }, 3000);
 
-    console.log(currentTime)
 }
