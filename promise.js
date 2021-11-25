5fb1943ca9d2a5236e2df7bd2224c6931bd886b5
Katrina Apel <kapel2810@gmail.com>
2021-11-25 11:42:23 +0200

    ex 2 done (almost)

--- promise.js
+++ promise.js
@@ -3,36 +3,66 @@
 ///////////////////////////////
 // ----------- ex 1 ----------
 
-const runCode = () => {
-    let minNumber = +prompt ('please enter min number')
-    let maxNumber = +prompt('please enter max number')
-
-    let myPromise = generate7BoomAfterDelayAsync(minNumber,maxNumber)
-    setTimeout(() => {
-        myPromise
-            .then((number) => console.log('success boom: ' + number))
-            .catch((number) => console.log('error boom: ' + number))
-    }, 1000)}
-
-    const generate7BoomAfterDelayAsync = (min, max) => {
-
-        const myPromise = new Promise((resolve, reject) => {
-            let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min
-
-            if (randomNumber % 7 === 0 || randomNumber % 10 === 7) {
-                resolve(randomNumber)
-            } else {
-                reject(randomNumber)
-            }
-
-        })
-        return myPromise
-    }
-
 // const runCode = () => {
+//     let minNumber = +prompt('please enter min number')
+//     let maxNumber = +prompt('please enter max number')
 
+//     let myPromise = generate7BoomAfterDelayAsync(minNumber, maxNumber)
+//     setTimeout(() => {
+//         myPromise
+//             .then((number) => console.log('success boom: ' + number))
+//             .catch((number) => console.log('error boom: ' + number))
+//     }, 1000)
 // }
 
-// const generatePrimeNumberAfterDelayAsync =() => {
+// const generate7BoomAfterDelayAsync = (min, max) => {
 
+//     const myPromise = new Promise((resolve, reject) => {
+//         let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min
+
+//         if (randomNumber % 7 === 0 || randomNumber % 10 === 7) {
+//             resolve(randomNumber)
+//         } else {
+//             reject(randomNumber)
+//         }
+
+//     })
+//     return myPromise
 // }
+
+////////////////////////////////
+// --------- ex 2 -----------
+
+//doesn't want to show error ???
+const runCode = () => {
+    alert("please enter two numbers from 1 to 100")
+    let minNumber = +prompt("enter first number")
+    let maxNumber = +prompt("enter second number")
+
+    let myPromise = generatePrimeNumberAfterDelayAsync(minNumber, maxNumber)
+
+    myPromise
+        .then((number) => console.log('succeess prime: ' +number))
+        .catch((number) => console.log('error! is not prime:' +number))
+}
+
+const generatePrimeNumberAfterDelayAsync = (min, max) => {
+    let myPromise = new Promise((resolve, reject) => {
+        let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min
+        let isRandom = true
+            for (let i = 2; i < randomNumber ; i++){
+                
+                if(randomNumber % i ===0) {
+                    isRandom = false
+                    break
+                }
+
+                if (isRandom === true){
+                    resolve(randomNumber)
+                } else {
+                    reject(randomNumber)
+                }
+            }
+    })
+    return myPromise
+}
